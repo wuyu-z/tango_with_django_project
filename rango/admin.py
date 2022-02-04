8de04df492ceecfda644368e20bd5ea02a857eb3
wuyu zhang <2651001Z@student.gla.ac.uk>
2022-02-04 06:41:54 +0000

    ch6

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,12 @@
 from dataclasses import field
 from django.contrib import admin
+from rango.models import Category,Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display=('title','category','url')
+    prepolulated_field={'slug':('name',)}
+
+admin.site.register(Category)
+admin.site.register(Page,PageAdmin)
 
 # Register your models here.
--- rango/models.py
+++ rango/models.py
@@ -1,16 +1,20 @@
 from turtle import title
 from unicodedata import category
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=128,unique=True)
     views=models.IntegerField(default=0)
     likes=models.IntegerField(default=0)
+    slug = models.SlugField()
 
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.name)
+        super(Category,self).save(*args,**kwargs)
     class Meta:
-        verbose_name_plural = 'Categories'
-        
+        verbose_name_plural = 'categories'
+    
     def __str__(self):
         return self.name
 
@@ -22,3 +26,4 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+# Create your models here.
